9c499533e54fa2d64b7157f10aee3bedb0a68359
Kevin Groat <kgroat@mail.umw.edu>
2011-03-21 20:08:04 -0400

    removed unnecessary stuff

--- installer/commands.sh
+++
@@ -1,5 +0,0 @@
-cd "/Users/kevingroat/Dropbox/CPSC 350 - Team 2 Folder/Recipe/First Sprint/installer/"
-echo 'using git'
-git clone git://github.com/emilyfhoffman/CPSC-350-team-2.git
-echo 'done using git'
-"mySQL" -u root -p < "/Users/kevingroat/Dropbox/CPSC 350 - Team 2 Folder/Recipe/First Sprint/installer/CPSC-350-team-2/recipes.sql"
