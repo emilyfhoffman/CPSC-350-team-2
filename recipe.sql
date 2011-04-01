3349818038979ff7b635fd0fe8fb63f8f2b3edc1
Kevin Groat <kgroat@mail.umw.edu>
2011-04-01 09:15:19 -0400

    New databases

---
+++ recipe.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 01, 2011 at 01:04 PM
+-- Server version: 5.1.44
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `recipe`
+--
+CREATE DATABASE IF NOT EXISTS 'recipe';
+GRANT ALL PRIVILEGES ON recipe.* to 'assist'@'localhost' identified by 'assist';
+USER 'recipe';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email_address` varchar(50) NOT NULL,
+  `recipe_id` int(11) NOT NULL,
+  `comment` blob NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `comments`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingredients`
+--
+
+CREATE TABLE IF NOT EXISTS `ingredients` (
+  `ingredient_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`ingredient_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `ingredients`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE IF NOT EXISTS `ratings` (
+  `email_address` varchar(50) NOT NULL,
+  `recipe_id` int(11) NOT NULL,
+  `rating` int(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ratings`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipe`
+--
+
+CREATE TABLE IF NOT EXISTS `recipe` (
+  `recipe_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email_address` varchar(50) NOT NULL,
+  `recipe_name` varchar(50) NOT NULL,
+  `instructions` blob NOT NULL,
+  `dish_type` enum('breakfast','appetizer','main dish','side dish','dessert','drink','other') NOT NULL DEFAULT 'other',
+  PRIMARY KEY (`recipe_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `recipe`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipe_to_ingredient`
+--
+
+CREATE TABLE IF NOT EXISTS `recipe_to_ingredient` (
+  `recipe_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  KEY `recipe_id` (`recipe_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `recipe_to_ingredient`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `email_address` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`email_address`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
