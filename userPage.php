6301a6591abc2d20d24b49b0ecd17f57847a12d7
Kevin Groat <kgroat@mail.umw.edu>
2011-04-05 19:54:30 -0400

    User page now properly links to recipes

--- userPage.php
+++ userPage.php
@@ -19,25 +19,31 @@
 	<?php
 	  include("db_connect.php");
 
-	   $id = $_GET['id'];
-	   $query = "select * from users where email_address = '$id';";
-	   $result = mysqli_query($db, $query);
-	   $row = mysqli_fetch_array($result);
+	    $id = $_GET['id'];
+	    $query = "select * from users where email_address = '$id';";
+	    $result = mysqli_query($db, $query);
+	    $row = mysqli_fetch_array($result);
 	   	$address = $row['email_address'];
 	   	echo "<h1>$address</h1> </br>";
 
-	   $query2 = "select * from recipes r inner join users u on r.email_address = u.email_address where u.email_address = '$id';";
-	   $result2 = mysqli_query($db, $query2);
-	   echo "<table border ='1'>";
-	   echo "<th>Recipes from this user</th>";
-	   while($row2 = mysqli_fetch_array($result2)){
-	   	echo "<tr><td>";
-   		$name = $row2['recipe_name'];
-		$id = $row['id'];
-		echo "<a href=\"recipe.php?id=$id\">$name</a>";
-		echo "</td></tr>";
-	   }
-	   echo "</table>";
+	    $query2 = "select * from recipes r inner join users u on r.email_address = u.email_address where u.email_address = '$id';";
+	    $result2 = mysqli_query($db, $query2);
+	    
+	    //Table start
+	    echo "<table border ='1'>";
+	   
+	    echo "<th>Recipes from this user</th>";
+	   
+	    while($row2 = mysqli_fetch_array($result2)){
+	   		echo "<tr><td>";
+   			$name = $row2['recipe_name'];
+			$id2 = $row2['recipe_id'];
+			echo "<a href=\"recipe.php?id=$id2\">$name</a>";
+			echo "</td></tr>";
+	   
+		}
+	   
+		echo "</table>";
    
   
 	?>
