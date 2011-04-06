7b5cf07be13444a5e251e8e5253f1d29fefb1162
emilyfhoffman <emilyfhoffman@comcast.net>
2011-04-06 17:56:54 -0400

    changed header_right

--- header_right.php
+++ header_right.php
@@ -49,18 +49,8 @@
       
     	?>
     	
-        <!--<li><a href="http://www.free-css.com/">Highest Rated Recipe</a></li>
-        <li><a href="http://www.free-css.com/">Second Highest</a></li>
-        <li><a href="http://www.free-css.com/">Third Highest</a></li>
-        <li><a href="http://www.free-css.com/">Fourth</a></li>
-        <li><a href="http://www.free-css.com/">Fifth</a></li>-->
       </ol>
-      <h2>RECIPE OF THE WEEK </h2>
-      <p><img src="images/photo3.jpg" alt="" width="183" height="70" /></p>
-      <p><strong>Recipe Name (24-12-2020</strong>)<br />
-        Decsription for awesome recipe.</p>
-      <!--<p><a target="_blank" href="http://validator.w3.org/check?uri=referer"><img height="31" alt="" src="http://www.w3.org/Icons/valid-xhtml10" width="88" vspace="8" border="0" /></a><a target="_blank" href="http://jigsaw.w3.org/css-validator/check/referer"><img alt="" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" vspace="8" border="0" /></a></p>-->
-    </div>
+     </div>
   </div>
     <div id="footer">Copyright Dish in a Flash- Designed by Team 2 Session 2</a></div>
 </div>
